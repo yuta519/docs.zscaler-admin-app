9314b9c88b5f8627cd2de332421487f18d0e0e21
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-10-17 17:58:30 +0900

    implement Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,24 @@
-function HomePage() {
-    return <div>Welcome to Next.js!</div>
+import Image from "next/image"
+import { Button } from '@material-ui/core'
+import { ButtonGroup } from '@material-ui/core'
+import { Typography } from '@material-ui/core'
+
+import Header from '@/components/Header'
+import profileimg from '@/public/profile.jpeg'
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <p>
+                <Typography variant="h4" component="div" gutterBottom>
+                    About Me
+                </Typography>
+
+                {/* <Image src="/profile.jpeg" width={400} height={512} alt="My avatar" /> */}
+                <Image src={profileimg} width={400} height={512} alt="My avatar" />
+            </p>
+        </>
+    )
 }
 
-export default HomePage
--- pages/zscaler-admin-app/index.tsx
+++ pages/zscaler-admin-app/index.tsx
@@ -1,5 +1,24 @@
-function HomePage() {
-    return <div>This is a zscaler admin help</div>
+import { Button, ButtonGroup } from '@material-ui/core'
+
+export default function Home() {
+  return (
+    <>
+      <div>This is a zscaler admin help</div>
+      <div style={{ margin: '0.5em' }}>
+        <Button variant="contained">Default</Button>{' '}
+        <Button variant="contained" color="primary">Primary</Button>{' '}
+        <Button variant="contained" color="secondary">Secondary</Button>{' '}
+        <Button variant="contained" disabled>Disabled</Button>{' '}
+        <Button variant="contained" color="primary" href="https://google.com/">LINK</Button>
+      </div>
+      <div style={{ margin: '0.5em' }}>
+        <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+          <Button>One</Button>
+          <Button>Two</Button>
+          <Button>Three</Button>
+        </ButtonGroup>
+      </div>
+    </>
+  )
 }
 
-export default HomePage
