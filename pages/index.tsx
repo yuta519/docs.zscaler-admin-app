47d712f0c27af4ea5024637686a6ef27592dfd20
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-10-17 18:10:08 +0900

    delete image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image"
-import { Button } from '@material-ui/core'
-import { ButtonGroup } from '@material-ui/core'
 import { Typography } from '@material-ui/core'
 
 import Header from '@/components/Header'
-import profileimg from '@/public/profile.jpeg'
+// import profileimg from '@/public/profile.jpeg'
 
 export default function Home() {
     return (
@@ -14,9 +12,7 @@ export default function Home() {
                 <Typography variant="h4" component="div" gutterBottom>
                     About Me
                 </Typography>
-
-                {/* <Image src="/profile.jpeg" width={400} height={512} alt="My avatar" /> */}
-                <Image src={profileimg} width={400} height={512} alt="My avatar" />
+                {/* <Image src={profileimg} width={400} height={512} alt="My avatar" /> */}
             </p>
         </>
     )
