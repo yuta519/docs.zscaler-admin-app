41d850f6e62c37d53a50e15b3c267a88a2cd8c29
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-10-22 09:10:41 +0900

    add career history graph

--- components/Careers.tsx
+++ components/Careers.tsx
@@ -17,18 +17,22 @@ export default function Careers() {
                         Backend Engineer - 2 years
                     </li>
                     <li>
-                        <Emoji emoji="computer" size={30} />&nbsp;
+                        <Emoji emoji="building_construction" size={30} />&nbsp;
                         Infra Engineer - 5 years
                     </li>
                     <li>
-                        <Emoji emoji="sparkles" size={30} />&nbsp;
-                        Sales Engineer - 5 years
+                        <Emoji emoji="lock" size={30} />&nbsp;
+                        Security Engineer - 5 years
+                    </li>
+                    <li>
+                        <Emoji emoji="heavy_dollar_sign" size={30} />&nbsp;
+                        Sales Engineer - 4 years
                     </li>
                 </ul>
 
 
                 <div className="p-4 mt-4 bg-gray-50">
-                    <h1 className="text-4xl text-center font-semibold mb-6">Package status</h1>
+                    <h3 className="text-4xl text-center font-semibold mb-6">Career Histroy</h3>
                     <div className="container">
                         <div className="flex flex-col md:grid grid-cols-12 text-gray-50">
 
@@ -42,9 +46,12 @@ export default function Careers() {
                                     </div>
                                 </div>
                                 <div className="bg-green-500 col-start-4 col-end-12 p-4 rounded-xl my-4 mr-auto shadow-md w-full">
-                                    <h3 className="font-semibold text-lg mb-1">Package Booked</h3>
+                                    <h3 className="font-semibold text-lg mb-1">Quick Work (Side Work)</h3>
+                                    <ul className="text-sm mb-1">
+                                        <li>as Backend Engineer - Python / FastAPI / Pytest / SqlAlchemy / PostgreSQL</li>
+                                    </ul>
                                     <p className="leading-tight text-justify w-full">
-                                        21 July 2021, 04:30 PM
+                                        July 2021 ~
                                     </p>
                                 </div>
                             </div>
@@ -59,14 +66,18 @@ export default function Careers() {
                                     </div>
                                 </div>
                                 <div className="bg-green-500 col-start-4 col-end-12 p-4 rounded-xl my-4 mr-auto shadow-md w-full">
-                                    <h3 className="font-semibold text-lg mb-1">Out for Delivery</h3>
+                                    <h3 className="font-semibold text-lg mb-1">Softbank (Main Work)</h3>
+                                    <ul className="text-sm mb-1">
+                                        <li>as Backend Engineer - Python / SqlAlchemy / PostgreSQL</li>
+                                        <li>as Infra Engineer - Linux</li>
+                                    </ul>
                                     <p className="leading-tight text-justify">
-                                        22 July 2021, 01:00 PM
+                                        January 2021 ~
                                     </p>
                                 </div>
                             </div>
 
-                            <div className="flex md:contents">
+                            {/* <div className="flex md:contents">
                                 <div className="col-start-2 col-end-4 mr-10 md:mx-auto relative">
                                     <div className="h-full w-6 flex items-center justify-center">
                                         <div className="h-full w-1 bg-red-500 pointer-events-none"></div>
@@ -82,6 +93,27 @@ export default function Careers() {
                                     </p>
                                 </div>
                             </div>
+ */}
+                            <div className="flex md:contents">
+                                <div className="col-start-2 col-end-4 mr-10 md:mx-auto relative">
+                                    <div className="h-full w-6 flex items-center justify-center">
+                                        <div className="h-full w-1 bg-gray-300 pointer-events-none"></div>
+                                    </div>
+                                    <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-gray-300 shadow text-center">
+                                        <i className="fas fa-exclamation-circle text-gray-400"></i>
+                                    </div>
+                                </div>
+                                <div className="bg-gray-300 col-start-4 col-end-12 p-4 rounded-xl my-4 mr-auto shadow-md w-full">
+                                    <h3 className="font-semibold text-lg mb-1 text-gray-400">CTC</h3>
+                                    <ul className="text-sm mb-1 text-gray-400">
+                                        <li>as Infra Engineer - VMware / AWS / Trend Micro / Zscaler / Splunk</li>
+                                        <li>as Sales Engineer - Trend Micro / Zscaler</li>
+                                    </ul>
+                                    <p className="leading-tight text-justify">
+                                        April 2017 ~ December 2020
+                                    </p>
+                                </div>
+                            </div>
 
                             <div className="flex md:contents">
                                 <div className="col-start-2 col-end-4 mr-10 md:mx-auto relative">
@@ -93,9 +125,12 @@ export default function Careers() {
                                     </div>
                                 </div>
                                 <div className="bg-gray-300 col-start-4 col-end-12 p-4 rounded-xl my-4 mr-auto shadow-md w-full">
-                                    <h3 className="font-semibold text-lg mb-1 text-gray-400">Delivered</h3>
+                                    <h3 className="font-semibold text-lg mb-1 text-gray-400">Interspace inc.</h3>
+                                    <ul className="text-sm mb-1 text-gray-400">
+                                        <li>as Infra Engineer - VMware / AWS / Trend Micro / Zscaler / Splunk</li>
+                                    </ul>
                                     <p className="leading-tight text-justify">
-
+                                        April 2017 ~ December 2020
                                     </p>
                                 </div>
                             </div>
@@ -103,6 +138,7 @@ export default function Careers() {
                         </div>
                     </div>
                 </div>
+
             </div>
         </>
 
