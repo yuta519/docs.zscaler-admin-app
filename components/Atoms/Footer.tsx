9dc28999b361900fb90c9918cf2a43e63b07bbb8
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-10-26 13:47:52 +0900

    chnage background color of footer

--- components/Atoms/Footer.tsx
+++ components/Atoms/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 function Footer() {
     return (
         <>
-            <footer className="py-6 dark:bg-coolGray-800 dark:text-coolGray-50">
+            <footer className="bg-gray-100 py-6 dark:bg-coolGray-800 dark:text-coolGray-50">
                 <div className="px-6 mx-auto space-y-6 divide-y divide-coolGray-400 md:space-y-12 divide-opacity-50">
                     <div className="grid grid-cols-12">
                         <div className="pb-6 col-span-full md:pb-0 md:col-span-6">
