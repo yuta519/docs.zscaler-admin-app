2f86f95b60c2c2d9d8a4f423db7556f0dca82b19
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-10-31 13:52:07 +0900

    change cast

--- pages/zscaler-admin-app/index.tsx
+++ pages/zscaler-admin-app/index.tsx
@@ -19,10 +19,12 @@ function ZscalerAdminApp() {
         <div className="w-screen">
           <p className="text-4xl mt-8">What is Zscaler Admin App?</p>
           <hr className="mb-4" />
-          <p className="text-xl">This is a repository to manage Zscaler Internet Access.</p>
-          <p className="text-xl">aaaa</p>
-          <p className="text-xl">aaaa</p>
-          <p className="text-xl">aaaa</p>
+          <div className="mx-4">
+            <p className="text-xl">This is a repository to manage Zscaler Internet Access.</p>
+            <p className="text-xl">aaaa</p>
+            <p className="text-xl">aaaa</p>
+            <p className="text-xl">aaaa</p>
+          </div>
         </div>
       </main>
       <Footer />
