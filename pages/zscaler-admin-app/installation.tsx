268f1b825767792552dca62aafdc3aa6d8c0b9cf
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-11-01 14:32:45 +0900

    add installation instructions

--- pages/zscaler-admin-app/installation.tsx
+++ pages/zscaler-admin-app/installation.tsx
@@ -19,13 +19,20 @@ function Installation() {
           <h2 className="text-4xl mt-8">Installation</h2>
           <hr className="mb-4" />
           <div className="mx-4">
-            <p className="text-xl">Use poetry</p>
-            <p className="text-xl">
-              Please install poetry
-            </p>
-            <p className="text-xl">
-              poetry install
-            </p>
+            <p className="text-base">To install this repository, please use Poetry</p>
+            <p className="text-sm">(What is a <a href="https://python-poetry.org/">Poetry</a>)</p>
+          </div>
+          <div className="mx-4 mt-4">
+            <p className="text-xl">Install Poetry (Optional)</p>
+            <p className="text-base">To install this repository, please use Poetry</p>
+            <p className="font-mono text-base text-white bg-black">$ curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -</p>
+          </div>
+          <div className="mx-4 mt-4">
+            <p className="text-xl">Install Zscaler Admin App</p>
+            <p className="text-base">Install this repository with Poetry</p>
+            <p className="font-mono text-base text-white bg-black">$ git clone https://github.com/yuta519/zscaler-admin-app.git</p>
+            <p className="font-mono text-base text-white bg-black">$ cd zscaler-admin-app</p>
+            <p className="font-mono text-base text-white bg-black">$ poetry shell & poetry install</p>
           </div>
         </div>
       </main>
