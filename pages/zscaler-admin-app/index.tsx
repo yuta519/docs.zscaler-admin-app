61214acfa4f9b48b7edf29e74371c21d3ee10bcb
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-11-13 13:12:36 +0900

    add repository URL

--- pages/zscaler-admin-app/index.tsx
+++ pages/zscaler-admin-app/index.tsx
@@ -23,6 +23,7 @@ function IndexPage() {
             <p className="text-xl">You could create Custom URL category, URL Filtering RUles, Admin User and many thins from CLI.</p>
             <p className="text-xl">Especially, if you are managing multi tenants of ZIA, you don't need to switch account by tenants.</p>
             <p className="text-xl">If you are intersted in this project, please contact me with ease.</p>
+            <p className="text-xl">If interested, please check<a href="https://github.com/yuta519/zscaler-admin-app">GitHub Repository</a></p>
           </div>
         </div>
       </main>
