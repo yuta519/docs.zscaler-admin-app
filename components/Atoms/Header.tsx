3826174d7bcebd7506d537ac08d3a51b3b0cc652
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-12-23 22:00:18 +0900

    erase unnecessary lines

--- components/Atoms/Header.tsx
+++ components/Atoms/Header.tsx
@@ -16,27 +16,14 @@ const Header: React.VFC = () => {
               <div>
                 <Link href="/">
                   <a className="flex items-center py-4 px-2">
-                    {/* <img src="logo.png" alt="Logo" className="h-8 w-8 mr-2"> */}
                     <span className="font-semibold text-gray-500 text-lg">Yuta Kawamura</span>
                   </a>
                 </Link>
-              </div>
-              <div className="hidden md:flex items-center space-x-1">
-                {/*
-                <a href="" className="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold ">Home</a>
-                <a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Services</a>
-                <a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">About</a>
-                <a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Contact Us</a>
-                */}
               </div>
             </div>
             <div className="hidden md:flex items-center space-x-3 ">
               <Link href="ja-jp">日本語</Link>
               <Link href="/">English</Link>
-              {/*
-              <a href="" className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-300">Log In</a>
-              <a href="" className="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300">Sign Up</a>
-            */}
             </div>
             <div className="md:hidden flex items-center">
               <button className="outline-none mobile-menu-button">
@@ -54,14 +41,6 @@ const Header: React.VFC = () => {
               </button>
             </div>
           </div>
-        </div>
-        <div className="hidden mobile-menu">
-          <ul className="">
-            <li className="active"><a href="index.html" className="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Home</a></li>
-            <li><a href="#services" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Services</a></li>
-            <li><a href="#about" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">About</a></li>
-            <li><a href="#contact" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Contact Us</a></li>
-          </ul>
         </div>
       </nav>
     </div>
