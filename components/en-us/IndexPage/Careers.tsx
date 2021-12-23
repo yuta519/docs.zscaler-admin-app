014d85c878354b3f5876d76c57e166e9484b7f36
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-12-23 22:12:30 +0900

    add new career

--- components/en-us/IndexPage/Careers.tsx
+++ components/en-us/IndexPage/Careers.tsx
@@ -33,6 +33,7 @@ const Careers: React.VFC = () => {
           <div className="container">
             <div className="flex flex-col md:grid grid-cols-12 text-gray-50">
 
+
               <div className="flex md:contents">
                 <div className="col-start-2 col-end-4 mr-10 md:mx-auto relative">
                   <div className="h-full w-6 flex items-center justify-center">
@@ -43,16 +44,18 @@ const Careers: React.VFC = () => {
                   </div>
                 </div>
                 <div className="bg-green-500 col-start-4 col-end-12 p-4 rounded-xl my-4 mr-auto shadow-md w-full">
-                  <h3 className="font-semibold text-lg mb-1">QuickWork, Inc. (Side Work)</h3>
+                  <h3 className="font-semibold text-lg mb-1">Innoscouter, Inc. (Side Work)</h3>
                   <ul className="text-sm mb-1">
-                    <li>Worked on web service as Backend Engineer</li>
-                    <li>Using server side skills like Python / FastAPI / Pytest / SqlAlchemy / PostgreSQL</li>
+                    <li>Worked on web service as Frontend and Backend Engineer</li>
+                    <li>Using frontend skills like Typescript / React</li>
+                    <li>Using server side skills like Ruby / Hanami / MySQL</li>
                   </ul>
                   <p className="leading-tight text-justify w-full">
                     July 2021 ~
                   </p>
                 </div>
               </div>
+
 
               <div className="flex md:contents">
                 <div className="col-start-2 col-end-4 mr-10 md:mx-auto relative">
@@ -72,6 +75,27 @@ const Careers: React.VFC = () => {
                   </ul>
                   <p className="leading-tight text-justify">
                     January 2021 ~
+                  </p>
+                </div>
+              </div>
+
+              <div className="flex md:contents">
+                <div className="col-start-2 col-end-4 mr-10 md:mx-auto relative">
+                  <div className="h-full w-6 flex items-center justify-center">
+                    <div className="h-full w-1 bg-gray-300 pointer-events-none"></div>
+                  </div>
+                  <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-gray-300 shadow text-center">
+                    <i className="fas fa-exclamation-circle text-gray-400"></i>
+                  </div>
+                </div>
+                <div className="bg-gray-300 col-start-4 col-end-12 p-4 rounded-xl my-4 mr-auto shadow-md w-full">
+                  <h3 className="font-semibold text-lg mb-1 text-gray-400">QuickWork, Inc. (Side Work)</h3>
+                  <ul className="text-sm mb-1 text-gray-400">
+                    <li>Worked on web service as Backend Engineer</li>
+                    <li>Using server side skills like Python / FastAPI / Pytest / SqlAlchemy / PostgreSQL</li>
+                  </ul>
+                  <p className="leading-tight text-justify">
+                    July 2021 ~ November 2021
                   </p>
                 </div>
               </div>
