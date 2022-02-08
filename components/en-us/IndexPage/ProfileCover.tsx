c78682621def75ec7468a96b5a2521a65708ad06
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-02-09 08:05:47 +0900

    add photo

--- components/en-us/IndexPage/ProfileCover.tsx
+++ components/en-us/IndexPage/ProfileCover.tsx
@@ -5,8 +5,8 @@ import ProfilePhoto from '@/public/profile.jpg'
 const ProfileCover: React.VFC = () => {
   return (
     <>
-      {/* <Image src={ProfilePhoto} objectFit="contain" /> */}
-      <img src='_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Fprofile.b774a23dfbf924eb56905471b0adfc4f.jpg&w=1920&q=75' />
+      <Image src={ProfilePhoto} objectFit="contain" />
+      {/* <img src='_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Fprofile.b774a23dfbf924eb56905471b0adfc4f.jpg&w=1920&q=75' /> */}
     </>
   )
 }
