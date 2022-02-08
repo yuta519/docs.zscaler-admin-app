be07f935c43f0028c4d0cfef0081531d6099aa6f
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-02-09 08:38:09 +0900

    improve profile photo

--- components/en-us/IndexPage/ProfileCover.tsx
+++ components/en-us/IndexPage/ProfileCover.tsx
@@ -1,13 +1,9 @@
-import Image from 'next/image';
-import ProfilePhoto from '@/public/profile.jpg'
-
 
 const ProfileCover: React.VFC = () => {
-  const myLoader = ({ src, width, quality }) => {
-    return `https://md-host-bucket.s3.us-east-2.amazonaws.com/${src}`
-  }
   return (
-    <Image loader={myLoader} src='profile.jpg' width={1000} height={450} objectFit="contain" />
+    <>
+      <img src='https://md-host-bucket.s3.us-east-2.amazonaws.com/profile.jpg' />
+    </>
   )
 }
 
