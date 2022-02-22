65bd694a1833bf00e417b840944dc0480a9600b8
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-02-22 10:35:25 +0900

    add sm to page/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function IndexPage() {
 
       <div className="grid grid-flow-col grid-cols-10">
         <div className="col-span-1"></div>
-        <div className="col-span-7 flex-col container mx-auto mt-9 px-8 justify-center">
+        <div className="col-span-7 flex-col mx-auto mt-9 pr-8 justify-center">
           <ProfileCover />
           <Icons />
           <AboutMe />
