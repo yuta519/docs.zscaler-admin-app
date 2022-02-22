f5b5c50fd6866532ea156c7895f91812634f8bfd
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-02-22 10:34:31 +0900

    add sm class for responsive design

--- components/en-us/BlogPage/index.tsx
+++ components/en-us/BlogPage/index.tsx
@@ -3,12 +3,9 @@ import Footer from '@/components/Atoms/Footer'
 import Blog from '@/components/Atoms/Blog'
 import RecentBlogs from '@/components/en-us/IndexPage/RecentBlogs'
 
-
 interface Props {
   readonly id: string | string[],
 }
-
-
 
 const BlogPage: React.FC<Props> = ({ id }: Props) => {
   return (
@@ -16,7 +13,7 @@ const BlogPage: React.FC<Props> = ({ id }: Props) => {
       <Header />
       <div className="grid grid-flow-col grid-cols-10">
         <div className="col-span-1"></div>
-        <div className="col-span-7 flex-col container mx-auto mt-9 px-8 justify-center">
+        <div className="col-span-7 flex-col container mx-auto mt-8 pr-8 sm:pr-2 justify-center">
           <Blog markdown_path={id} />
         </div>
         <div className="col-span-2 pr-5">
@@ -27,6 +24,5 @@ const BlogPage: React.FC<Props> = ({ id }: Props) => {
     </>
   )
 }
-
 
 export default BlogPage
