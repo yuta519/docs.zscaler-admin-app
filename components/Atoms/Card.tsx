d2a50efa035d7495c484704672371663a3f497dc
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-03-08 07:20:59 +0900

    change card link

--- components/Atoms/Card.tsx
+++ components/Atoms/Card.tsx
@@ -9,9 +9,9 @@ interface Props {
 
 const Card: React.FC<Props> = ({ img, cardName, cardDescription, tags }: Props) => {
   return (
-    <Link href="/projects/">
-      <div key={cardName} className="flex-1 m-2 max-w-xs rounded overflow-hidden shadow-lg cursor-pointer">
-        <img className="w-full" src={img} alt="Sunset in the mountains" />
+    <Link key={cardName} href="/projects/">
+      <div className="flex-1 m-2 max-w-xs rounded overflow-hidden shadow-lg cursor-pointer">
+        <img key={cardName} className="w-full" src={img} alt="Sunset in the mountains" />
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{cardName}</div>
           <p className="text-gray-700 text-base">{cardDescription}</p>
@@ -19,7 +19,9 @@ const Card: React.FC<Props> = ({ img, cardName, cardDescription, tags }: Props) 
         <div className="px-6 pt-4 pb-2">
           {tags.map((tag) => {
             return (
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{tag}</span>
+              <span key={tag} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #{tag}
+              </span>
             )
           })}
         </div>
