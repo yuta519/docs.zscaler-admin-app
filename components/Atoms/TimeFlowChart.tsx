bac2ef975686d2f815891f6a6df19e67d618b3b0
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-03-16 07:51:59 +0900

    make chart component

--- components/Atoms/TimeFlowChart.tsx
+++ components/Atoms/TimeFlowChart.tsx
@@ -3,10 +3,12 @@ import { useState } from "react";
 
 import Modal from "@/components/Atoms/Modal";
 
-// interface Line {
-//   isActive: string;
-//   description: string;
-// }
+interface elements {
+  name: string;
+  isActive: boolean;
+  title: string;
+  startDate: string;
+}
 
 // interface Props {
 //   readonly visibility: boolean;
@@ -14,9 +16,62 @@ import Modal from "@/components/Atoms/Modal";
 //   readonly lines: Line[];
 // }
 
+const Chart = ({ name, isActive, title, startDate }: elements) => {
+  if (isActive) {
+    return (
+      <div className="flex md:contents">
+        <div className="col-start-1 col-end-3 mr-10 md:mx-auto relative">
+          <div className="h-full w-6 flex items-center justify-center">
+            <div className="h-full w-1 bg-green-500 pointer-events-none"></div>
+          </div>
+          <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-green-500 shadow text-center">
+            <i className="fas fa-check-circle text-white"></i>
+          </div>
+        </div>
+        <div className="bg-green-500 col-start-3 col-end-12 p-4 rounded-xl my-4 mr-auto shadow-md w-full">
+          <h3 className="font-bold md:text-xl sm:text-base mb-1 text-black">
+            {name}
+          </h3>
+          <ul className="text-sm mb-1 text-gray-800">
+            <li className="md:text-base sm:text-xs">{title}</li>
+          </ul>
+          <p className="leading-tight font-semibold text-justify w-full text-gray-600">
+            {startDate}
+          </p>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex md:contents">
+        <div className="col-start-1 col-end-3 mr-10 md:mx-auto relative">
+          <div className="h-full w-6 flex items-center justify-center">
+            <div className="h-full w-1 bg-gray-300 pointer-events-none"></div>
+          </div>
+          <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-gray-300 shadow text-center">
+            <i className="fas fa-exclamation-circle text-gray-400"></i>
+          </div>
+        </div>
+        <div className="bg-gray-300 col-start-3 col-end-12 p-4 rounded-xl my-4 mr-auto shadow-md w-full">
+          <h3 className="font-semibold md:text-lg sm:text-base mb-1 text-gray-400">
+            {name}
+          </h3>
+          <ul className="text-sm mb-1 text-gray-400">
+            <li className="md:text-base sm:text-xs">{title}</li>
+          </ul>
+          <p className="leading-tight text-justify">{startDate}</p>
+        </div>
+      </div>
+    );
+  }
+};
+
 const TimeFlowChart: React.FC = () => {
   return (
     <div className="flex flex-col md:grid grid-cols-12 text-gray-50 mt-3">
+      {/* <Chart name="name" isActive={true} title="title" startDate="startDate" />
+      <Chart name="name" isActive={false} title="title" startDate="startDate" /> */}
+
       <div className="flex md:contents">
         <div className="col-start-1 col-end-3 mr-10 md:mx-auto relative">
           <div className="h-full w-6 flex items-center justify-center">
