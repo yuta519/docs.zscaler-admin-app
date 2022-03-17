027a59d5c3bae7520daf21881c770f5589c9bd3f
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-03-17 10:45:51 +0900

    add more button to projects

--- components/en-us/IndexPage/Projects.tsx
+++ components/en-us/IndexPage/Projects.tsx
@@ -1,4 +1,6 @@
-import Card from '@/components/Atoms/Card';
+import Link from "next/link";
+
+import Card from "@/components/Atoms/Card";
 
 const Projects: React.VFC = () => {
   return (
@@ -6,29 +8,31 @@ const Projects: React.VFC = () => {
       <p className="font-mono font-bold sm:text-base md:text-3xl">Projects</p>
       <hr className="my-2" />
 
-      <div className='my-5 md:flex'>
+      <div className="my-5 md:flex">
         <Card
-          img='https://raw.githubusercontent.com/yuta519/yuta519.github.io/main/public/projects/yuta519.github.io.png'
-          cardName='yuta519.github.io (This website)'
-          cardDescription='My profile site. Including not only profile, but also blogs I wrote.'
-          tags={['Next.js', 'Typescript']}
+          img="https://raw.githubusercontent.com/yuta519/yuta519.github.io/main/public/projects/yuta519.github.io.png"
+          cardName="yuta519.github.io (This website)"
+          cardDescription="My profile site. Including not only profile, but also blogs I wrote."
+          tags={["Next.js", "Typescript"]}
         />
         <Card
-          img='https://raw.githubusercontent.com/yuta519/yuta519.github.io/main/public/projects/Notion_app_logo.png'
-          cardName='Notion API SDK'
-          cardDescription='Notion SDK for Golang.'
+          img="https://raw.githubusercontent.com/yuta519/yuta519.github.io/main/public/projects/Notion_app_logo.png"
+          cardName="Notion API SDK"
+          cardDescription="Notion SDK for Golang."
           tags={["Go", "Notion API"]}
         />
         <Card
-          img='https://raw.githubusercontent.com/yuta519/yuta519.github.io/main/public/projects/Zscaler.png'
-          cardName='Zscaler Python SDK'
-          cardDescription='Zscaler SDK for Python.'
+          img="https://raw.githubusercontent.com/yuta519/yuta519.github.io/main/public/projects/Zscaler.png"
+          cardName="Zscaler Python SDK"
+          cardDescription="Zscaler SDK for Python."
           tags={["Pyhon", "Zscaler"]}
         />
       </div>
-      {/* <p className='text-blue-400 md:text-base sm:text-xs'><Link href="/projects">more</Link></p> */}
+      <p className="text-blue-400 md:text-xl sm:text-base">
+        <Link href="/projects">more</Link>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
