9d155dfae3e754dd39e5da6115cedd0ec27903e4
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-03-20 10:03:49 +0900

    display project name when clicking project card at top page

--- components/en-us/ProjectPage/index.tsx
+++ components/en-us/ProjectPage/index.tsx
@@ -1,25 +1,30 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
+
+import Header from "@/components/Atoms/Header";
+import Footer from "@/components/Atoms/Footer";
 
 interface Propos {
-  readonly projectName: string | string[],
+  readonly projectName: string | string[];
 }
 
 const Projects = ({ projectName }: Propos) => {
-  const [projectDetail, setProjectDetail] = useState("")
+  const [projectDetail, setProjectDetail] = useState("");
   useEffect(() => {
-    const projectJson = require('./projects.json')
-    setProjectDetail(projectJson)
-  }, [])
-  console.log(projectDetail)
+    const projectJson = require("./projects.json");
+    setProjectDetail(projectJson);
+  }, []);
+  console.log(projectDetail);
   return (
-    <div className="grid grid-flow-col grid-cols-10">
-      <div className="col-span-1"></div>
-      <div className="col-span-7 flex-col container mx-auto mt-8 pr-8 sm:pr-2 justify-center">
+    <>
+      <Header />
+      <div className="grid grid-flow-col grid-cols-10">
+        <div className="col-span-1">{projectName}</div>
+        <div className="col-span-7 flex-col container mx-auto mt-8 pr-8 sm:pr-2 justify-center"></div>
+        <div className="col-span-2 pr-5"></div>
       </div>
-      <div className="col-span-2 pr-5">
-      </div>
-    </div>
-  )
-}
+      <Footer />
+    </>
+  );
+};
 
-export default Projects
+export default Projects;
--- components/en-us/ProjectsPage/AllProjects.tsx
+++ components/en-us/ProjectsPage/AllProjects.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react"
 import Card from "@/components/Atoms/Card"
-import link from "next/link"
 
 interface Project {
   readonly name: string,
