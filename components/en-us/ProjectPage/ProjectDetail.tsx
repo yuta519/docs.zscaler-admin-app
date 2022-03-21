31b2c4efef76702bafc2469e3115652991840d09
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-03-21 15:32:03 +0900

    add project detail page

---
+++ components/en-us/ProjectPage/ProjectDetail.tsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+
+import RecentBlogs from "@/components/en-us/IndexPage/RecentBlogs";
+
+interface project {
+  readonly id: string;
+  readonly name: string;
+  readonly shortDescription: string;
+  readonly description: string;
+  readonly imageUrl: string;
+  readonly relatedBlogs: string[];
+  readonly usingStacks: string[];
+}
+
+interface Props {
+  readonly projectDetail: project;
+}
+
+const ProjectDetail = ({ projectDetail }: Props) => {
+  if (projectDetail === undefined) {
+  }
+  console.log(projectDetail);
+  return (
+    <div className="grid grid-flow-col grid-cols-10">
+      <div className="md:col-span-1 md:block sm:hidden sm:invisible"></div>
+      <div className="flex-col mt-9 md:col-span-7 md:pr-8 sm:col-span-10">
+        {projectDetail === undefined ? (
+          "loading"
+        ) : (
+          <div className="p-4">
+            <p className="font-mono font-bold sm:text-base md:text-3xl">
+              {projectDetail.name}
+            </p>
+            <hr className="my-2" />
+
+            <div className="mt-8 pl-4">
+              <p className="font-mono font-bold sm:text-xl md:text-2xl">
+                Description
+              </p>
+              <hr />
+              <div className="flex justify-left space-x-4 lg:py-10 lg:col-end-13 sm:text-base md:text-xl">
+                {projectDetail.description}
+              </div>
+            </div>
+
+            <div className="mt-8 pl-4">
+              <p className="font-mono font-bold sm:text-base md:text-xl">
+                Image
+              </p>
+              <hr />
+              <div className="flex justify-left space-x-4 lg:py-10 lg:col-end-13">
+                <img
+                  key={projectDetail.imageUrl}
+                  className="w-full"
+                  src={projectDetail.imageUrl}
+                  alt="Sunset in the mountains"
+                />
+              </div>
+            </div>
+
+            <div className="mt-8 pl-4">
+              <p className="font-mono font-bold sm:text-xl md:text-2xl">
+                Stacks
+              </p>
+              <hr />
+
+              <ul className="lg:py-10 lg:col-end-13 sm:text-base md:text-xl">
+                {projectDetail.usingStacks.map((stack) => {
+                  return <li>{stack}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="md:pr-5 md:col-span-2 md:block sm:invisible sm:hidden">
+        {/* TODO: change to related blogs */}
+        <RecentBlogs />
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetail;
--- components/en-us/ProjectPage/index.tsx
+++ components/en-us/ProjectPage/index.tsx
@@ -2,26 +2,25 @@ import { useEffect, useState } from "react";
 
 import Header from "@/components/Atoms/Header";
 import Footer from "@/components/Atoms/Footer";
+import ProjectDetail from "@/components/en-us/ProjectPage/ProjectDetail";
 
 interface Props {
   readonly projectName: string | string[];
 }
 
 const Projects = ({ projectName }: Props) => {
-  const [projectDetail, setProjectDetail] = useState("");
+  const [projectDetail, setProjectDetail] = useState();
   useEffect(() => {
     const projectJson = require("./projects.json");
-    setProjectDetail(projectJson);
+    const project = projectJson.find(
+      (projects: { id: string | string[] }) => projects.id == projectName
+    );
+    setProjectDetail(project);
   }, []);
-  console.log(projectDetail);
   return (
     <>
       <Header />
-      <div className="grid grid-flow-col grid-cols-10">
-        <div className="col-span-1">{projectName}</div>
-        <div className="col-span-7 flex-col container mx-auto mt-8 pr-8 sm:pr-2 justify-center"></div>
-        <div className="col-span-2 pr-5"></div>
-      </div>
+      <ProjectDetail projectDetail={projectDetail} />
       <Footer />
     </>
   );
