dda1a666da7fa26e0f47b10b069b4a51ba80e03e
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-03-22 09:01:44 +0900

    erase chart component

--- components/Atoms/TimeFlowChart.tsx
+++ components/Atoms/TimeFlowChart.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 
+import Chart from "@/components/Atoms/Chart";
 import Modal from "@/components/Atoms/Modal";
 
 interface element {
@@ -14,71 +15,37 @@ interface Props {
   readonly elements: element[];
 }
 
-const Chart = ({ name, isActive, title, duration }: element) => {
-  if (isActive) {
-    return (
-      <div className="flex md:contents">
-        <div className="col-start-1 col-end-3 mr-10 md:mx-auto relative">
-          <div className="h-full w-6 flex items-center justify-center">
-            <div className="h-full w-1 bg-green-500 pointer-events-none"></div>
-          </div>
-          <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-green-500 shadow text-center">
-            <i className="fas fa-check-circle text-white"></i>
-          </div>
-        </div>
-        <div className="bg-green-500 col-start-3 col-end-12 p-4 rounded-xl my-4 mr-auto shadow-md w-full">
-          <h3 className="font-bold md:text-xl sm:text-base mb-1 text-gray-800">
-            {name}
-          </h3>
-          <ul className="text-sm mb-1 text-gray-800">
-            <li className="md:text-base sm:text-xs">{title}</li>
-          </ul>
-          <p className="leading-tight font-semibold text-justify w-full text-gray-600">
-            {duration}
-          </p>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="flex md:contents">
-        <div className="col-start-1 col-end-3 mr-10 md:mx-auto relative">
-          <div className="h-full w-6 flex items-center justify-center">
-            <div className="h-full w-1 bg-gray-300 pointer-events-none"></div>
-          </div>
-          <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-gray-300 shadow text-center">
-            <i className="fas fa-exclamation-circle text-gray-400"></i>
-          </div>
-        </div>
-        <div className="bg-gray-300 col-start-3 col-end-12 p-4 rounded-xl my-4 mr-auto shadow-md w-full">
-          <h3 className="font-semibold md:text-lg sm:text-base mb-1 text-gray-400">
-            {name}
-          </h3>
-          <ul className="text-sm mb-1 text-gray-400">
-            <li className="md:text-base sm:text-xs">{title}</li>
-          </ul>
-          <p className="leading-tight text-justify">{duration}</p>
-        </div>
-      </div>
-    );
-  }
-};
-
 const TimeFlowChart: React.FC<Props> = ({ elements }: Props) => {
+  const [selectedCareer, setSelectedCareer] = useState("");
+  const [modalVisibility, setModalVisibility] = useState(false);
+
+  const handleOpenCareerDialog = (modalTitle: string) => {
+    setModalVisibility(true);
+    setSelectedCareer(modalTitle);
+  };
+
+  const handleCloseCareerDialog = () => {
+    setModalVisibility(false);
+    setSelectedCareer("");
+  };
+
   return (
     <div className="flex flex-col md:grid grid-cols-12 text-gray-50 mt-3">
       {elements.map((element: element) => (
-        <>
-          <Chart
-            key={element.name + "-chart"}
-            name={element.name}
-            isActive={element.isActive}
-            title={element.title}
-            duration={element.duration}
-          />
-          <Modal visibility={false} title={element.name} />
-        </>
+        <Chart
+          key={element.name + "-chart"}
+          name={element.name}
+          isActive={element.isActive}
+          title={element.title}
+          duration={element.duration}
+          onClick={handleOpenCareerDialog}
+        />
       ))}
+      <Modal
+        visibility={modalVisibility}
+        title={selectedCareer}
+        onClose={handleCloseCareerDialog}
+      />
     </div>
   );
 };
