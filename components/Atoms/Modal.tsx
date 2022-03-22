1425100808e5e86f44eebf1fb4c509ff7f929dd7
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-03-22 09:01:13 +0900

    add prpos of onClose

--- components/Atoms/Modal.tsx
+++ components/Atoms/Modal.tsx
@@ -3,9 +3,10 @@ import React, { useState } from "react";
 interface Props {
   readonly visibility: boolean;
   readonly title: string;
+  readonly onClose: () => void;
 }
 
-const Modal: React.FC<Props> = ({ visibility, title }: Props) => {
+const Modal: React.FC<Props> = ({ visibility, title, onClose }: Props) => {
   if (visibility) {
     return (
       <div
@@ -49,13 +50,8 @@ const Modal: React.FC<Props> = ({ visibility, title }: Props) => {
             <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
               <button
                 type="button"
-                className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-              >
-                Deactivate
-              </button>
-              <button
-                type="button"
                 className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                onClick={onClose}
               >
                 Cancel
               </button>
