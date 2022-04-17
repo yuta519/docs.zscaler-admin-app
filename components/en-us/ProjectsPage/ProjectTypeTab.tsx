0d1ea0a0695c62c3d6a6efb927ebb23719955c5d
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-04-17 13:39:16 +0900

    add ProjectTypeTab.tsx to display tabs

---
+++ components/en-us/ProjectsPage/ProjectTypeTab.tsx
@@ -0,0 +1,27 @@
+const ProjectTypeTab = () => {
+  return (
+    <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+      <ul className="flex flex-wrap -mb-px justify-center">
+        <li className="mr-2">
+          <a
+            href="#"
+            className="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+          >
+            Private
+          </a>
+        </li>
+        <li className="mr-2">
+          <a
+            href="#"
+            className="inline-block p-4 text-blue-600 rounded-t-lg border-b-2 border-blue-600 active dark:text-blue-500 dark:border-blue-500"
+            aria-current="page"
+          >
+            Work / Group
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ProjectTypeTab;
--- components/en-us/ProjectsPage/index.tsx
+++ components/en-us/ProjectsPage/index.tsx
@@ -1,15 +1,17 @@
-import Header from '@/components/Atoms/Header'
-import Footer from '@/components/Atoms/Footer'
-import AllProjects from '@/components/en-us/ProjectsPage/AllProjects'
+import Header from "@/components/Atoms/Header";
+import Footer from "@/components/Atoms/Footer";
+import AllProjects from "@/components/en-us/ProjectsPage/AllProjects";
+import ProjectTypeTab from "@/components/en-us/ProjectsPage/ProjectTypeTab";
 
 const Projects: React.FC = () => {
   return (
     <>
       <Header />
+      <ProjectTypeTab />
       <AllProjects />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
