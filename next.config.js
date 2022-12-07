ca3885e546a74f8fde961e75b8a10b00937ceb85
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-12-07 10:01:31 +0900

    use next/Image

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  images: {
+    domains: [
+      'uploads-ssl.webflow.com',
+      'static.wixstatic.com',
+      'cryptogames.co.jp',
+      'group.softbank',
+      'encrypted-tbn0.gstatic.com',
+      'www.ctc-g.co.jp',
+      'www.interspace.ne.jp',
+      'quickwork.jp'
+    ],
+  },
+};
